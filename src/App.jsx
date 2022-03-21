de3d730f6b20f4fff3c491f827b7fbf1312b148b
Fr33yr <75025925+Fr33yr@users.noreply.github.com>
2022-03-21 16:40:11 -0300

    sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,100 @@
 import React,{ Fragment } from 'react';
-import './App.css';
+import './styles/main.css';
+
+
+
+const calcBtns = [
+  {
+    id: "zero",
+    text: "0"
+  },
+  {
+    id: "one",
+    text: "1"
+  },
+  {
+    id: "two",
+    text: "2"
+  },
+  {
+    id: "three",
+    text: "3"
+  },
+  {
+    id: "four",
+    text: "4"
+  },
+  {
+    id: "five",
+    text: "5"
+  },
+  {
+    id: "six",
+    text: "6"
+  },
+  {
+    id: "seven",
+    text: "7"
+  },
+  {
+    id: "eight",
+    text: "8"
+  },
+  {
+    id: "nine",
+    text: "9"
+  },
+  {
+    id: "decimal",
+    text: "."
+  },
+  {
+    id: "add",
+    text: "+"
+  },
+  {
+    id: "subtract",
+    text: "-"
+  },
+  {
+    id: "multiply",
+    text: "*"
+  },
+  {
+    id: "divide",
+    text: "/"
+  },
+  {
+    id: "clear",
+    text: "AC"
+  }
+]
 
 function App() {
   return (
     
     <Fragment>
-      <h1>Hello world</h1>
+      <h1>Calculator</h1>
+      <div className="display"></div>
+      <div className="btns-container">
+        {calcBtns.map((btn) => (
+          <Buttons btn={btn} key={btn.id} id={btn.id} />
+        ))}
+      </div>
     </Fragment>
   );
 }
 
 export default App;
+
+
+export function Buttons({btn}){
+
+  return(
+    <Fragment>
+        <div className="button" id={btn.id}>
+          <p>{btn.text}</p>
+        </div>
+    </Fragment>
+  );
+}
