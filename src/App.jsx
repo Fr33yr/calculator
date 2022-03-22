290c58dc7e56ec62fc4bf4a209f0f77f4f8f9bdb
Fr33yr <75025925+Fr33yr@users.noreply.github.com>
2022-03-22 11:54:28 -0300

    stateless version

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import React,{ Fragment } from 'react';
-import './styles/main.css';
+import React,{ Fragment } from "react";
+import './styles/main.css'
 
 
-export default function App() {
+export default function App(){
 
   return(
     <Fragment>
       <div id="frame">
-        <div className="output-screen">
-          
+        <div className="display">
+          <input type="text" className="history" />
+          <input type="text" className="current-calc" />
+        </div>
+        <div className="buttons">
+          <button className="span-two">AC</button>
+          <button>DEL</button>
+          <button>/</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>X</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>-</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+          <button>.</button>
+          <button className="span-two">0</button>
+          <button>%</button>
         </div>
       </div>
     </Fragment>
